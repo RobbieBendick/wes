66a67d13063e5943104016c46624dd46a300a540
mageiden <60712721+mageiden@users.noreply.github.com>
2020-12-21 18:52:45 -0800

    sidebar code revamp

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,81 +1,36 @@
 import React from "react";
 import "./Sidebar.css";
 
+function ListItem({ title, href }) {
+  return (
+    <li>
+      <h5 className="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
+        <a className="nav-link sidebar-title" href={href}>
+          {title}
+        </a>
+      </h5>
+    </li>
+  );
+}
+
 function Sidebar() {
+  //  sidebar needs the specific addonitem that it's clicking's toggle variable, and if toggle is true
+  //     if toggle is false, setToggle(true)
+
   return (
     <nav className="navbar-expand-lg d-none d-md-block sidebar">
       <div className="sidebar-sticky sidebar-content">
-        <h5 className="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
-          <a className="nav-link sidebar-title" href="#map">
-            Map
-          </a>
-        </h5>
         <ul className="nav flex-column mb-2">
-          <ul className="nav flex-column">
-            <li className="nav-item">
-              <h5 className="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
-                <a className="nav-link sidebar-title" href="#chat">
-                  Chat
-                </a>
-              </h5>
-            </li>
-            <li>
-              <h5 className="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
-                <a className="nav-link sidebar-title" href="#raidframes">
-                  Raidframes
-                </a>
-              </h5>
-            </li>
-            <li>
-              <h5 className="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
-                <a className="nav-link sidebar-title" href="#unitframes">
-                  Unitframes
-                </a>
-              </h5>
-            </li>
-            <li>
-              <h5 className="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
-                <a className="nav-link sidebar-title" href="#actionbar">
-                  Actionbar
-                </a>
-              </h5>
-            </li>
-            <li>
-              <h5 className="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
-                <a className="nav-link sidebar-title" href="#questing">
-                  Questing
-                </a>
-              </h5>
-            </li>
-            <li>
-              <h5 className="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
-                <a className="nav-link sidebar-title" href="#textures">
-                  Textures/Icons
-                </a>
-              </h5>
-            </li>
-            <li>
-              <h5 className="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
-                <a className="nav-link sidebar-title" href="#minimap">
-                  Minimap
-                </a>
-              </h5>
-            </li>
-            <li>
-              <h5 className="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
-                <a className="nav-link sidebar-title" href="#buffs">
-                  Buffs/Debuffs
-                </a>
-              </h5>
-            </li>
-            <li>
-              <h5 className="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
-                <a className="nav-link sidebar-title" href="#swingtimer">
-                  Swingtimer
-                </a>
-              </h5>
-            </li>
-          </ul>
+          <ListItem title="Map" href="map" />
+          <ListItem title="Chat" href="chat" />
+          <ListItem title="Raidframes" href="raidframes" />
+          <ListItem title="Unitframes" href="unitframes" />
+          <ListItem title="Actionbar" href="actionbar" />
+          <ListItem title="Questing" href="questing" />
+          <ListItem title="Textures/Icons" href="textures" />
+          <ListItem title="Minimap" href="minimap" />
+          <ListItem title="Buffs/Debuffs" href="buffs" />
+          <ListItem title="Swingtimer" href="swingtimer" />
         </ul>
       </div>
     </nav>
